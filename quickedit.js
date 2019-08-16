8a0baf26f9b84be3d441559689d56c7dfd80e03e
Luke <luke-mml@users.noreply.github.com>
2019-08-16 11:00:46 +1000

    tidy up comments

--- quickedit.js
+++ quickedit.js
@@ -10,10 +10,6 @@
         // Node/CommonJS
         module.exports = function (root, jQuery) {
             if (jQuery === undefined) {
-                // require('jQuery') returns a factory that requires window to
-                // build a jQuery instance, we normalize how we use modules
-                // that require this pattern but the window provided is a noop
-                // if it's defined (how jquery works)
                 if (typeof window !== 'undefined') {
                     jQuery = require('jquery');
                 } else {
@@ -149,12 +145,12 @@
                 });
             }
         }
-
+        // execute formatting command and set focus
         function applyFormat(sCmd, sValue) {
             document.execCommand(sCmd, false, sValue);
             $this.focus();
         }
-
+        // set and get option
         function option(key, val) {
             if (val) {
                 options[key] = val;
